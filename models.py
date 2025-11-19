a21cf3b6e5d6bcd26ea05165d817ffa31e233df1
DumbotwinOG <leon.weir@education.nsw.gov.au>
2025-11-20 09:49:31 +1100

    structured the codes and added base.html

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ db = SQLAlchemy()
 class Cards(db.Model):
     card_name = db.Column(db.Integer, primary_key=True)
     card_rarity = db.Column(db.Text)
-    card_rating = db.Column(db.Rating, nullable=False)
+    card_rating = db.Column(db.Integer, nullable=False)
     elixir_cost = db.Column(db.Integer, nullable=False)
     card_type = db.Column(db.Boolean, default=False)
     attack_type = db.Column(db.DateTime, default=datetime.utcnow)
